0c12b3ee45d3d8480cc3293a5f637cdd8c75fb65
feng <fengxb1104@gmail.com>
2022-10-05 10:57:14 +0800

    使用faker批量生成博客数据 实现简单分页 修复了首页视图中不显示文章创建时间的问题

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,15 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('posts/<int:pk>/', views.detail, name='dateil'),
-    path('archives/<int:year>/<int:month>/', views.archive, name='archive'),
-    path('categories/<int:pk>/', views.category, name='category'),
-    path('tags/<int:pk>/', views.tag, name='tag'),
+    # path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    # path('posts/<int:pk>/', views.detail, name='dateil'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='dateil'),
+    # path('archives/<int:year>/<int:month>/', views.archive, name='archive'),
+    path('archives/<int:year>/<int:month>/', views.ArchiveView.as_view(), name='archive'),
+    # path('categories/<int:pk>/', views.category, name='category'),
+    path('categories/<int:pk>/', views.CategoryView.as_view(), name='category'),
+    # path('tags/<int:pk>/', views.tag, name='tag'),
+    path('tag/<int:pk>/', views.TagView.as_view(), name='tag'),
 ]
+
--- blog/views.py
+++ blog/views.py
@@ -4,50 +4,105 @@ import markdown
 from django.shortcuts import render, get_object_or_404
 from django.utils.text import slugify
 from markdown.extensions.toc import TocExtension
+from django.views.generic import ListView, DetailView
 
 from .models import Post, Category, Tag
 
 # Create your views here.
-def index(request):
-    # post_list = Post.objects.all().order_by('-created_time')
-    post_list = Post.objects.all()
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+    # 开启分页功能
+    paginate_by = 10
 
-def detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+# def index(request):
+#     # post_list = Post.objects.all().order_by('-created_time')
+#     post_list = Post.objects.all()
+#     return render(request, 'blog/index.html', context={'post_list': post_list})
 
-    # 阅读量 +1
-    post.increase_views()
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
 
-    md = markdown.Markdown(extensions=[
-        'markdown.extensions.extra',
-        'markdown.extensions.codehilite',
-        # 'markdown.extensions.toc',
-        TocExtension(slugify=slugify),
-    ])
-    post.body = md.convert(post.body)
+    def get(self, request, *args, **kwargs):
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
 
-    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
-    post.toc = m.group(1) if m is not None else ''
+        self.object.increase_views()
+        return response
 
-    return render(request, 'blog/detail.html', context={'post': post})
+    def get_object(self, queryset=None):
+        post = super(PostDetailView, self).get_object(queryset=None)
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            TocExtension(slugify=slugify)
+        ])
+        post.body = md.convert(post.body)
+
+        m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+        post.toc = m.group(1)
+
+        return post
+
+# def detail(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#
+#     # 阅读量 +1
+#     post.increase_views()
+#
+#     md = markdown.Markdown(extensions=[
+#         'markdown.extensions.extra',
+#         'markdown.extensions.codehilite',
+#         # 'markdown.extensions.toc',
+#         TocExtension(slugify=slugify),
+#     ])
+#     post.body = md.convert(post.body)
+#
+#     m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+#     post.toc = m.group(1) if m is not None else ''
+#
+#     return render(request, 'blog/detail.html', context={'post': post})
 
 
-def archive(request, year, month):
-    post_list = Post.objects.filter(created_time__year=year,
-                                    created_time__month=month
-                                    ).order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+class ArchiveView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
 
-def category(request, pk):
-    cate = get_object_or_404(Category, pk=pk)
-    post_list = Post.objects.filter(category=cate).order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+    def get_queryset(self):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        return super(ArchiveView, self).get_queryset().filter(created_time__year=year,
+                                                              created_time__month=month)
 
-def tag(request, pk):
-    t = get_object_or_404(Tag, pk=pk)
-    post_list = Post.objects.filter(tags=t).order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+# def archive(request, year, month):
+#     post_list = Post.objects.filter(created_time__year=year,
+#                                     created_time__month=month
+#                                     ).order_by('-created_time')
+#     return render(request, 'blog/index.html', context={'post_list': post_list})
+
+class CategoryView(IndexView):
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
+
+
+class TagView(IndexView):
+    def get_queryset(self):
+        t = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super(TagView, self).get_queryset().filter(category=t)
+
+# def category(request, pk):
+#     cate = get_object_or_404(Category, pk=pk)
+#     post_list = Post.objects.filter(category=cate).order_by('-created_time')
+#     return render(request, 'blog/index.html', context={'post_list': post_list})
+#
+# def tag(request, pk):
+#     t = get_object_or_404(Tag, pk=pk)
+#     post_list = Post.objects.filter(tags=t).order_by('-created_time')
+#     return render(request, 'blog/index.html', context={'post_list': post_list})
 
 
 
